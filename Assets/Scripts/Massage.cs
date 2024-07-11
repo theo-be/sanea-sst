1e29674311b0440c88557ccc1dda0c9dbab915fe
theo-be <belliere.theo@outlook.fr>
2024-07-11 17:02:56 +0200

    debut massage

--- Assets/Scripts/Massage.cs
+++ Assets/Scripts/Massage.cs
@@ -1,35 +1,198 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using Unity.XR.CoreUtils;
 using UnityEngine;
+using UnityEngine.XR.OpenXR.Features.Interactions;
 
 public class Massage : MonoBehaviour
 {
     
-    public GameObject corps;
-    public XROrigin rig;
-    public GameObject sphere;
     public GameObject mainGauche;
     public GameObject mainDroite;
+
+    public EtatMassage etatMassage = EtatMassage.HorsMassage;
+
+    // tolerance d'ecart par rapport a la cible
+    public GameObject zoneDeTolerance;
+    public float toleranceDeRotation = 20f;
+    
+    private float intervalleMinPulsation = .4f;
+    private float intervalleMaxPulsation = .7f;
+
+    private float impulsionMin;
+    private float impultionMax;
+
+    // ecart entre les deux : 0.05y
+    public GameObject debutImpulsion;
+    public GameObject finImpulsion;
+
+    private ChangeMaterial _changeMaterial;
+
+    
+    // ecran debug
+    public GameObject ecranDebug;
+    private  TMP_Text texteDebug;
+    private const string mainsenPosition = "mains en position";
+    private const string mainsPasenPosition = "mains pas en position";
+
+    private float tempsAvantDebutMassage = 0f;
+
+    private bool hautTouche = true;
+    private bool basTouche = false;
+
+    private int nombreImpulsion = 0;
+
+    public int nombreImpulsionsParSerie = 20;
+    
+
+    public void changerEtat(EtatMassage etat)
+    {
+        etatMassage = etat;
+    }
+    
+    private bool MainsEnPosition()
+    {
+        // verifier si les mains sont environ dans la zone de tolerance avant le massage
+
+        var posMainGauche = mainGauche.transform.position;
+        var posMainDroite = mainDroite.transform.position;
+        var collisionSphere = zoneDeTolerance.transform.localScale;
+        var centreSphere = zoneDeTolerance.transform.position;
+
+        // rotation cible : 270 gauche 90 droite axe z
+        
+        var rotMainGauche = mainGauche.transform.eulerAngles;
+        var rotMainDroite = mainDroite.transform.eulerAngles;
+
+        if (
+            // main gauche
+            Mathf.Abs((posMainGauche - centreSphere).magnitude) < collisionSphere.magnitude / 2f
+            && Mathf.Abs(rotMainGauche.z - 270f) < toleranceDeRotation
+            // main droite
+            &&
+            Mathf.Abs((posMainDroite - centreSphere).magnitude) < collisionSphere.magnitude / 2f
+            && Mathf.Abs(rotMainDroite.z - 90f) < toleranceDeRotation
+            )
+        {
+            return true;
+        }
+        
+        
+        return false;
+    }
+
+    private bool MainDansLaZone(GameObject main, GameObject zone)
+    {
+        Vector3 positionMain = main.transform.position;
+        Vector3 tailleZone = zone.transform.localScale;
+        Vector3 positionZone = zone.transform.position;
+
+        if (
+            positionMain.x >= positionZone.x - tailleZone.x / 2f
+            && positionMain.x <= positionZone.x + tailleZone.x / 2f
+            && positionMain.z >= positionZone.z - tailleZone.z / 2f
+            && positionMain.z <= positionZone.z + tailleZone.z / 2f
+        )
+        {
+            return true;
+        }
+        
+        return false;
+    }
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _changeMaterial = zoneDeTolerance.GetComponent<ChangeMaterial>();
+        texteDebug = ecranDebug.GetComponent<TMP_Text>();
     }
-
-    private bool MainsEnPosition()
-    {
-        // verifier si les mains sont environ dans la bonne position pour le massage, avant et pendant
-        return false;
-    }
-
-
-
+    
     // Update is called once per frame
     void Update()
     {
-        Vector3 position = (mainGauche.transform.position - mainDroite.transform.position) / 2.0f;
-        sphere.transform.position = mainGauche.transform.position + position;
+        
+        if (etatMassage == EtatMassage.Preparation)
+        {
+            if (MainsEnPosition())
+            {
+
+                if (tempsAvantDebutMassage == 0f)
+                {
+                    tempsAvantDebutMassage = Time.time;
+                }
+                else
+                {
+                    texteDebug.text += "debut de l'exercice dans : " + (Time.time - tempsAvantDebutMassage);
+                }
+                
+                
+                debutImpulsion.transform.position = mainGauche.transform.position;
+                finImpulsion.transform.position = debutImpulsion.transform.position + Vector3.down * 0.05f;
+                
+
+                zoneDeTolerance.SetActive(false);
+                _changeMaterial.SetOtherMaterial();
+                texteDebug.text = mainsenPosition;
+
+                if (Time.time - tempsAvantDebutMassage > 1f)
+                {
+                    etatMassage = EtatMassage.EnCours;
+                    texteDebug.text = "Massage en cours";
+                }
+
+            }
+            else
+            {
+                zoneDeTolerance.SetActive(true);
+                _changeMaterial.SetOriginalMaterial();
+                tempsAvantDebutMassage = 0f;
+                texteDebug.text = mainsPasenPosition;
+            }
+        } else if (etatMassage == EtatMassage.EnCours)
+        {
+            // detecter une impulsion
+
+            Vector3 positionDebutImpulsion = debutImpulsion.transform.position;
+            Vector3 positionFinImpulsion = finImpulsion.transform.position;
+
+            if (MainDansLaZone(mainGauche, debutImpulsion) && MainDansLaZone(mainDroite, debutImpulsion) || true)
+            {
+                if (hautTouche)
+                {
+                    if (mainGauche.transform.position.y < positionFinImpulsion.y)
+                    {
+                        hautTouche = false;
+                        basTouche = true;
+                    }
+                }
+                else
+                {
+                    if (mainGauche.transform.position.y > positionDebutImpulsion.y)
+                    {
+                        hautTouche = true;
+                        basTouche = false;
+                        nombreImpulsion++;
+                        texteDebug.text = "Nombre d'impulsions : " + nombreImpulsion;
+                    }
+                }
+            }
+            
+        }
+
+
+
+
+
+        //Console.WriteLine("position main gauche : " + mainGauche.transform.position.ToString());
+        //Debug.Log(mainGauche.transform);
+
     }
+    
+}
+
+public enum EtatMassage
+{
+    HorsMassage,
+    Preparation,
+    EnCours
 }
